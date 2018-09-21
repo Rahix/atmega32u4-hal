d1038529096bbb06fdcb47ed6b3ad50893696a70
Rahix <rahix@rahix.de>
2018-09-21 14:46:07 +0200

    Add timer4 pwm
    Signed-off-by: Rahix <rahix@rahix.de>

--- src/timer.rs
+++ src/timer.rs
@@ -123,3 +123,30 @@ timer! {
         }),
     ]
 }
+
+// Timer4
+timer! {
+    Info: (Timer4Pwm, TIMER4, tim),
+    Init: {
+        // Prescale/64
+        tim.tccr_b.modify(|_, w| unsafe { w.cs().bits(0b0111) });
+        // Set WGM to Phase-Correct PWM Mode
+        tim.tccr_d.modify(|_, w| unsafe { w.wgm().bits(0b01) });
+    },
+    Pins: [
+        |portc, PC7, pwm, dc| ({
+            // Use OCR_A as Duty Cycle
+            // Enable PWM for OCR_A
+            pwm.tim.tccr_a.modify(|_, w| unsafe { w.com_a().bits(0b10).pwm_a().set_bit() });
+        }, {
+            unsafe { (*atmega32u4::TIMER4::ptr()).ocr_a.write(|w| w.bits(dc)); }
+        }),
+        |portd, PD7, pwm, dc| ({
+            // Use OCR_D as Duty Cycle
+            // Enable PWM for OCR_D
+            pwm.tim.tccr_c.modify(|_, w| unsafe { w.com_d().bits(0b10).pwm_d().set_bit() });
+        }, {
+            unsafe { (*atmega32u4::TIMER4::ptr()).ocr_d.write(|w| w.bits(dc)); }
+        }),
+    ]
+}
